33051e6613a53503b013c7e57c67c9fc1288e392
adamjkwong <adamjkwong@gmail.com>
2021-06-07 17:09:05 -0700

    first java commit

---
+++ javaFun/FirstJavaProgram.java
@@ -0,0 +1,8 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("My name is Adam Wong");
+	System.out.println("I am 100 years old");
+	System.out.println("my hometown is Burbank, CA");
+    }
+}
+
