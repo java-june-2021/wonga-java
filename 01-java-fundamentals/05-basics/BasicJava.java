2e7b4cc82539aeb5944924bdbc6ab1d4ed7b1c0c
adamjkwong <adamjkwong@gmail.com>
2021-06-09 17:03:31 -0700

    basic java incremental save

---
+++ 01-java-fundamentals/05-basics/BasicJava.java
@@ -0,0 +1,26 @@
+public class BasicJava {
+    public void challenge1(){
+//         for (int i = 1; i < 256; i++){
+//             System.out.println(i);
+//         }
+// }
+//     public void challenge2(){
+//         for (int i = 1; i < 256; i++){
+//             if (i%2!=0){
+//                 System.out.println(i);
+//             }
+//         }
+//     }
+//     public void challenge3(){
+//         int sum = 0;
+//         for (int i = 1; i < 256; i++){
+//             sum = i + sum;
+//             System.out.println("New Number: "+i+ " Sum: " + sum);
+//         }
+//     }
+    public void challenge4(the_array){
+        for (int i = 1; i < the_array.length; i++){
+            System.out.println(the_array[i]);
+        }
+    }
+// }
---
+++ 01-java-fundamentals/05-basics/BasicJavaTest.java
@@ -0,0 +1,10 @@
+public class BasicJavaTest {
+    public static void main(String[] args) {
+        BasicJava challenger = new BasicJava();
+        // challenger.challenge1();
+        // challenger.challenge2();
+        // challenger.challenge3();
+        int x = {1,3,5,7,9,13};
+        challenger.challenge4(x);
+    }
+}
