8bed60e1222b37dd0e59b0416d29ba008897405c
adamjkwong <adamjkwong@gmail.com>
2021-06-13 16:02:45 -0700

    finished hashmatique

--- 01-java-fundamentals/07-map-of-the-hashmatique/Hashmatique.java
+++ 01-java-fundamentals/07-map-of-the-hashmatique/Hashmatique.java
@@ -1,25 +1,19 @@
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Random;
 import java.util.Set;
-
+import java.util.HashMap;
 
 public class Hashmatique {
-    public static void oneSong(){
-        HashMap<String, String> musicMap = new HashMap<String, String>();
-        musicMap.put("Lover Song", "I love love love you so much");
-        musicMap.put("The Dance Party", "Sam Samurai");
-        musicMap.put("Rock Your Socks", "Walter Wizard");
-        musicMap.put("The Gang Finds a Shoe", "Walter Wizard");
-        musicMap.put("Why So Much Water", "Walter Wizard");
-        System.out.println(musicMap.get("Lover Song"));
-    }
-        // public void allSongs(){
-        //     Set<String> keys = musicHashMap.keySet();
-        //     for(String key : keys) {
-        //         System.out.println(key);
-        //         System.out.println(musicHashMap.get(key)); 
-        //     }
-        // }
+        public void oneSong() {
+            HashMap<String, String> musicMap = new HashMap<String, String>();
+            musicMap.put("Lover Song", "I love love love you so much");
+            musicMap.put("The Dance Party", "It's a party and I'll dance if I want to");
+            musicMap.put("Rock Your Socks", "We will, we will, rock socks");
+            musicMap.put("The Gang Finds a Shoe", "What with this shoe");
+            musicMap.put("Why So Much Water", "Water water everywhere");
+            System.out.println(musicMap.get("Lover Song"));
+            Set<String> keys = musicMap.keySet();
+            for(String key : keys) {
+                System.out.println(musicMap.get(key));    
+            } 
+        }
 }
---
+++ 01-java-fundamentals/07-map-of-the-hashmatique/HashmatiqueTest.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashMap;
+
+public class HashmatiqueTest {
+    public static void main(String[] args) {
+        Hashmatique theCollection = new Hashmatique();
+        theCollection.oneSong();
+    }
+}
