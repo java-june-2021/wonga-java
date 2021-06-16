55cad5a012a5ba4c710cda417d313db01e8c86b2
adamjkwong <adamjkwong@gmail.com>
2021-06-16 09:07:36 -0700

    incremental save on project assignment

---
+++ 02-java-oop/01-project-class/Project.java
@@ -0,0 +1,52 @@
+public class Project {
+    public String name;
+    public String description;
+
+    // Constructor
+    public Project(String name,String description){
+        this.name = name;
+        this.description = description;
+    }
+
+    // Method Overloading
+    public Project() {
+        this.name = "Generic Name";
+        this.description = "Generic Description";
+    }
+
+    public Project(String name) {
+        this.name = name;
+        this.description = "Generic Description";
+    }
+
+    //Revisit the input paramtera here. 
+    public void elevatorPitch(String name, String description){
+        System.out.println(this.name + " : "+ this.description);
+    }
+
+    //Methods go here
+        // Getters and Setters
+    public String getName(){
+        return this.name;
+    }
+
+    public String getDescription(){
+        return this.description;
+    }
+
+    public void setName(String name){
+        if(name.equals("")){
+            System.out.println("Name must be more than 1 character");
+            return;
+        }
+        if(name.length() > 4){
+            System.out.println("must be at least 4 characertrs");
+        }
+        this.name = name;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+}
---
+++ 02-java-oop/01-project-class/ProjectTest.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+public class ProjectTest{
+    public static void main(String[] args) {
+        Project projectOne = new Project("Project One", "The first project");
+        Project projectTwo = new Project("Project Two");
+        Project projectThree = new Project();
+        System.out.println(projectOne.name);
+        System.out.println(projectTwo.description);
+        System.out.println(projectThree.description);
+        //Figure out how to invoke elevatorPitch
+        System.out.println();
+    }
+}
