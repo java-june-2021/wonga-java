66e5bc4cc21591b0ecff9751ef89e20f462ac494
adamjkwong <adamjkwong@gmail.com>
2021-06-30 09:42:58 -0700

    got help from TA to finish this assignment

--- 03-java-spring/02-spring-data-i/02-show/theshow/src/main/java/com/adamwong/theshow/models/Book.java
+++ 03-java-spring/02-spring-data-i/02-show/theshow/src/main/java/com/adamwong/theshow/models/Book.java
@@ -32,7 +32,49 @@ public class Book {
     
     public Book() {
     }
-    public Book(String title, String desc, String lang, int pages) {
+    public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getLanguage() {
+		return language;
+	}
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+	public Integer getNumberOfPages() {
+		return numberOfPages;
+	}
+	public void setNumberOfPages(Integer numberOfPages) {
+		this.numberOfPages = numberOfPages;
+	}
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	public Book(String title, String desc, String lang, int pages) {
         this.title = title;
         this.description = desc;
         this.language = lang;
